e12e878db72fa5cac6b1fa1b268fd857f9e7b35f
marisofip <marisofipalencia@gmail.com>
2022-12-01 22:48:27 +0000

    lista

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+function listas() {
+  let laLista = [];
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; y < noun.length; z++) {
+        laLista.push(pronoun[x] + adj[y] + noun[z] + ".com");
+      }
+    }
+  }
+  document.getElementById("lista").innerHTML = laLista.join("<br>");
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  listas();
 };
